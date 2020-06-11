0bfda11be45e98a65cc50495b23d2e449c61e1b8
Phuong Nhi <phuongnhi4599@gmail.com>
2020-06-11 18:11:41 +0700

    update cate

--- highlands-coffee/public/slide.js
+++ highlands-coffee/public/slide.js
@@ -12,4 +12,29 @@ $('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
     }
     next.children(':first-child').clone().appendTo($(this));
   }
+
+  var header = document.getElementById("size");
+  var btns = header.getElementsByClassName("btn");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+    });
+  }
+  function myFunction1() {
+    document.getElementById("myDIV").style.display = 'block';
+    document.getElementById("content").style.display = 'none';
+    document.getElementById("myDIV2").style.display = 'none';
+  }
+  function myFunction2() {
+    document.getElementById("myDIV2").style.display = 'block';
+    document.getElementById("content").style.display = 'none';
+    document.getElementById("myDIV").style.display = 'none'
+  }
+  function myFunction() {
+    document.getElementById("content").style.display = 'block';
+    document.getElementById("myDIV2").style.display = 'none';
+    document.getElementById("myDIV").style.display = 'none';
+  }
 });
--- highlands-coffee/src/components/Menu/Tea.js
+++ highlands-coffee/src/components/Menu/Tea.js
@@ -6,40 +6,43 @@ class Tea extends Component {
     return (
       <div className="tra">
         <div className="cate">
-          {/* <h1 className="title">trà</h1> */}
-          <div className="col-8">
-            <Link to="/chi-tiet-tra">
-              <img src="https://www.highlandscoffee.com.vn/vnt_upload/product/03_2018/menu-TEA-inside.jpg" alt="Trà"/>
-            </Link>
-            <p>Hương vị tự nhiên, thơm ngon của Trà Việt với phong cách hiện đại tại Highlands Coffee sẽ giúp bạn gợi mở vị giác của bản thân và tận hưởng một cảm giác thật khoan khoái, tươi mới.</p>
-            <Link to="/chi-tiet-tra" className="hang">xem sản phẩm</Link><br/><br/><br/>
-          </div>
-
-          <div className="col-4">
-            <div className="item">
-              <div className="anh">
-                <Link to="/chi-tiet-tra">
-                  <img src="https://www.highlandscoffee.com.vn/vnt_upload/product/03_2018/thumbs/270_crop_TRASENVANG.png" alt="Trà Sen Vàng"/>
-                </Link>
-              </div>
-              <div className="caption">
-                <h4>trà sen vàng</h4>
-                <span>Thức uống chinh phục những thực khách khó tính! Sự kết hợp độc đáo giữa trà Ô long</span>
-              </div>
+          <h1 className="title">trà</h1>
+          <div className="vang">
+            <div className="col-8">
+              <Link to="/chi-tiet-tra">
+                <img src="https://www.highlandscoffee.com.vn/vnt_upload/product/03_2018/menu-TEA-inside.jpg" alt="Trà"/>
+              </Link>
+              <p>Hương vị tự nhiên, thơm ngon của Trà Việt với phong cách hiện đại tại Highlands Coffee sẽ giúp bạn gợi mở vị giác của bản thân và tận hưởng một cảm giác thật khoan khoái, tươi mới.</p>
+              <Link to="/chi-tiet-tra" className="hang">xem sản phẩm</Link>
             </div>
 
-            <div className="item">
-              <div className="anh">
-                <Link to="/chi-tiet-tra">
-                  <img src="https://www.highlandscoffee.com.vn/vnt_upload/product/03_2018/thumbs/270_crop_TRATHACHDAO.png" alt="Trà Thạch Đào"/>
-                </Link>
+            <div className="col-4">
+              <div className="item">
+                <div className="anh">
+                  <Link to="/chi-tiet-tra">
+                    <img src="https://www.highlandscoffee.com.vn/vnt_upload/product/03_2018/thumbs/270_crop_TRASENVANG.png" alt="Trà Sen Vàng"/>
+                  </Link>
+                </div>
+                <div className="caption">
+                  <h4>trà sen vàng</h4>
+                  <span>Thức uống chinh phục những thực khách khó tính! Sự kết hợp độc đáo giữa trà Ô long</span>
+                </div>
               </div>
-              <div className="caption">
-                <h4>trà thạch đào</h4>
-                <span>Vị trà đậm đà kết hợp cùng những miếng đào thơm ngon mọng nước cùng thạch đào</span>
+
+              <div className="item">
+                <div className="anh">
+                  <Link to="/chi-tiet-tra">
+                    <img src="https://www.highlandscoffee.com.vn/vnt_upload/product/03_2018/thumbs/270_crop_TRATHACHDAO.png" alt="Trà Thạch Đào"/>
+                  </Link>
+                </div>
+                <div className="caption">
+                  <h4>trà thạch đào</h4>
+                  <span>Vị trà đậm đà kết hợp cùng những miếng đào thơm ngon mọng nước cùng thạch đào</span>
+                </div>
               </div>
             </div>
-          </div>
+          
+          </div> 
         </div>
 
 
